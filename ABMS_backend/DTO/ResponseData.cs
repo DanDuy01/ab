754ba15cf06f4cd407e8b2ca41c1695c4132d7c4
hhanhh1 <hahaianh2041@gmail.com>
2024-02-20 12:57:29 +0700

    environment config

---
+++ ABMS_backend/DTO/ResponseData.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace ABMS_backend.DTO
+{
+    public class ResponseData<T>
+    {
+        public T? Data { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public string? ErrMsg { get; set; }
+    }
+}
