972721bb705cc7c4e4e26157c2de528b20aebb36
Truong Trong Sang (k15 HL) <sangtthe153078@fpt.edu.vn>
2024-02-23 16:42:12 +0000

    Update CmbAccountManagementService.cs

--- ABMS_backend/Services/CmbAccountManagementService.cs
+++ ABMS_backend/Services/CmbAccountManagementService.cs
@@ -1,60 +1,152 @@
-﻿using ABMS_backend.DTO;
+using ABMS_backend.DTO;
 using ABMS_backend.Models;
 using ABMS_backend.Repositories;
 using ABMS_backend.Utils.Validates;
 using System.Net;
+using AutoMapper;
+using ABMS_backend.Utils.Exceptions;
+using System.Collections.Generic;
 
 namespace ABMS_backend.Services
 {
     public class CmbAccountManagementService : ICmbAccountManagementRepository
-    { 
+    {
         private readonly abmsContext _abmsContext;
+        private IMapper _mapper;
 
-        public CmbAccountManagementService(abmsContext abmsContext)
+        public CmbAccountManagementService(abmsContext abmsContext, IMapper mapper)
         {
             _abmsContext = abmsContext;
+            _mapper = mapper;
         }
 
         ResponseData<string> ICmbAccountManagementRepository.createCmbAccount(AccountForInsertDTO dto)
-
         {
-            Account account = new Account();
-            account.ApartmentId = dto.apartmentId;
-            account.PhoneNumber = dto.phone;
-            account.PasswordSalt = dto.pwd_salt;
-            account.PasswordHash = dto.pwd_hash;
-            account.Email = dto.email;
-            account.FullName = dto.full_name;
-            account.Avatar = dto.avatar;
-            account.Id = Guid.NewGuid().ToString();
-            account.CreateUser = "admin";
-            account.CreateTime = DateTime.Now;
-            account.Status = (int)Constants.STATUS.ACTIVE;
-            _abmsContext.Accounts.Add(account);
-            _abmsContext.SaveChanges();
-            return new ResponseData<string>
+            //validate
+            string error = dto.Validate();
+
+            if (error != null)
             {
-                Data = account.Id,
-                StatusCode = HttpStatusCode.OK,
-                ErrMsg = ErrorApp.SUCCESS.description
-            };
+                return new ResponseData<string>
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    ErrMsg = error
+                };
+            }
+
+            try
+            {
+                Account account = new Account();
+                account.Id = Guid.NewGuid().ToString();
+                account.ApartmentId = dto.apartmentId;
+                account.PhoneNumber = dto.phone;
+                account.PasswordSalt = dto.pwd_salt;
+                account.PasswordHash = dto.pwd_hash;
+                account.Email = dto.email;
+                account.FullName = dto.full_name;
+                account.Avatar = dto.avatar;
+                account.CreateUser = "admin";
+                account.CreateTime = DateTime.Now;
+                account.Status = (int)Constants.STATUS.ACTIVE;
+                _abmsContext.Accounts.Add(account);
+                _abmsContext.SaveChanges();
+                return new ResponseData<string>
+                {
+                    Data = account.Id,
+                    StatusCode = HttpStatusCode.OK,
+                    ErrMsg = ErrorApp.SUCCESS.description
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseData<string>
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    ErrMsg = "Created failed why " + ex.Message
+                };
+            }
+
+        }
+
+        ResponseData<string> ICmbAccountManagementRepository.updateCmbAccount(string id, AccountForInsertDTO dto)
+        {
+            //validate
+            string error = dto.Validate();
+
+            if (error != null)
+            {
+                return new ResponseData<string>
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    ErrMsg = error
+                };
+            }
+
+            try
+            {
+                Account account = _abmsContext.Accounts.Find(id);
+                if (account == null)
+                {
+                    throw new CustomException(ErrorApp.OBJECT_NOT_FOUND);
+                }
+                account.ApartmentId = dto.apartmentId;
+                account.PhoneNumber = dto.phone;
+                account.PasswordSalt = dto.pwd_salt;
+                account.PasswordHash = dto.pwd_hash;
+                account.Email = dto.email;
+                account.FullName = dto.full_name;
+                account.Avatar = dto.avatar;
+                account.ModifyUser = "admin";
+                account.ModifyTime = DateTime.Now;
+                _abmsContext.Accounts.Update(account);
+                _abmsContext.SaveChanges();
+                return new ResponseData<string>
+                {
+                    Data = account.Id,
+                    StatusCode = HttpStatusCode.OK,
+                    ErrMsg = ErrorApp.SUCCESS.description
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseData<string>
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    ErrMsg = "Updated failed why " + ex.Message
+                };
+            }
         }
 
         ResponseData<string> ICmbAccountManagementRepository.deleteCmbAccount(string id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Account account = _abmsContext.Accounts.Find(id);
+                if (account == null)
+                {
+                    throw new CustomException(ErrorApp.OBJECT_NOT_FOUND);
+                }
+                account.Status = (int)Constants.STATUS.IN_ACTIVE;
+                account.ModifyUser = "admin";
+                account.ModifyTime = DateTime.Now;
+                _abmsContext.Accounts.Update(account);
+                _abmsContext.SaveChanges();
+                return new ResponseData<string>
+                {
+                    Data = account.Id,
+                    StatusCode = HttpStatusCode.OK,
+                    ErrMsg = ErrorApp.SUCCESS.description
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseData<string>
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    ErrMsg = "Deleted failed why " + ex.Message
+                };
+            }
         }
-
-        List<ResponseData<Account>> ICmbAccountManagementRepository.getCmbAccount(AccountForSearchDTO dto)
-        {
-            throw new NotImplementedException();
-        }
-
-        ResponseData<Account> ICmbAccountManagementRepository.getCmbAccountById(string id)
-        {
-            throw new NotImplementedException();
-        }
-
 
         ResponseData<List<Account>> ICmbAccountManagementRepository.getCmbAccount(AccountForSearchDTO dto)
         {
@@ -73,8 +165,21 @@ namespace ABMS_backend.Services
             };
         }
 
+        ResponseData<Account> ICmbAccountManagementRepository.getCmbAccountById(string id)
         {
-            throw new NotImplementedException();
+            Account account = _abmsContext.Accounts.Find(id);
+            if (account == null)
+            {
+                throw new CustomException(ErrorApp.OBJECT_NOT_FOUND);
+            }
+            return new ResponseData<Account>
+            {
+                Data = account,
+                StatusCode = HttpStatusCode.OK,
+                ErrMsg = ErrorApp.SUCCESS.description
+            };
         }
+
+
     }
 }
