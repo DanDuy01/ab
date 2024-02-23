2530adf48ad84c95212e6d1897811866edf4e988
Truong Trong Sang (k15 HL) <sangtthe153078@fpt.edu.vn>
2024-02-23 16:39:13 +0000

    Update ICmbAccountManagementRepository.cs

--- ABMS_backend/Repositories/ICmbAccountManagementRepository.cs
+++ ABMS_backend/Repositories/ICmbAccountManagementRepository.cs
@@ -1,20 +1,17 @@
-﻿using ABMS_backend.DTO;
+using ABMS_backend.DTO;
 using ABMS_backend.Models;
 
 namespace ABMS_backend.Repositories
 {
     public interface ICmbAccountManagementRepository
     {
-
-
         ResponseData<string> createCmbAccount(AccountForInsertDTO dto);
 
         ResponseData<string> updateCmbAccount(string id, AccountForInsertDTO dto);
 
-
         ResponseData<string> deleteCmbAccount(string id);
 
-        List<ResponseData<Account>> getCmbAccount(AccountForSearchDTO dto);
+        ResponseData<List<Account>> getCmbAccount(AccountForSearchDTO dto);
 
         ResponseData<Account> getCmbAccountById(string id);
     }
