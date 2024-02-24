fb18c20f30767fed7c0b7bde9e24c9c6043d7e0a
hhanhh1 <hahaianh2041@gmail.com>
2024-02-24 13:27:42 +0700

    reservation

---
+++ ABMS_backend/Repositories/IReservationManagement.cs
@@ -0,0 +1,20 @@
+﻿using ABMS_backend.DTO;
+using ABMS_backend.Models;
+
+namespace ABMS_backend.Repositories
+{
+    public interface IReservationManagement
+    {
+        ResponseData<string> createReservation(ReservationForInsertDTO dto);
+
+        ResponseData<string> updateReservation(string id, ReservationForInsertDTO dto);
+
+        ResponseData<string> deleteReservation(string id);
+
+        ResponseData<List<Utility>> getAllReservation(UtilityForSearch dto);
+
+        ResponseData<List<UtilityForInsertDTO>> getReservation(UtilityForSearch dto);
+
+        ResponseData<Utility> getReservationById(string id);
+    }
+}
