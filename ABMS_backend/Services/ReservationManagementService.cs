aa581d4e94f5388f32267a875ce1517da92dc8bc
D4ng <53222130+hainamzsd@users.noreply.github.com>
2024-03-09 00:48:18 +0700

    modify get reservation by id

--- ABMS_backend/Services/ReservationManagementService.cs
+++ ABMS_backend/Services/ReservationManagementService.cs
@@ -224,12 +224,18 @@ namespace ABMS_backend.Services
 
         public ResponseData<ReservationResponseDTO> getReservationById(string id)
         {
-            UtilitySchedule utilitySchedule = _abmsContext.UtilitySchedules.Find(id);
+            var utilitySchedule = _abmsContext.UtilitySchedules
+                        .Where(us => us.Id == id)
+                        .Include(us => us.UtilityDetail)
+                            .ThenInclude(ud => ud.Utility)
+                        .FirstOrDefault();
             if (utilitySchedule == null)
             {
                 throw new CustomException(ErrorApp.OBJECT_NOT_FOUND);
             }
             ReservationResponseDTO dto = new ReservationResponseDTO();
+            dto.id = utilitySchedule.Id;
+            dto.utility_detail_id = utilitySchedule.UtilityDetail.UtilityId;
             dto.room_id = utilitySchedule.RoomId;
             dto.utility = utilitySchedule.UtilityDetail.Utility.Name;
             dto.utility_detail_name = utilitySchedule.UtilityDetail.Name;
