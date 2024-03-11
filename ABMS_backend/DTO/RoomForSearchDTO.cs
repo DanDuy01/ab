7c7343aedeb7147d7f97da59adefc05b072821b5
haidang <machaidangms@gmail.com>
2024-03-11 11:11:00 +0700

    small change

--- ABMS_backend/DTO/RoomForSearchDTO.cs
+++ ABMS_backend/DTO/RoomForSearchDTO.cs
@@ -2,8 +2,8 @@
 {
     public class RoomForSearchDTO
     {
-        public string accountId { get; set; }
-        public string buildingId { get; set; }
+        public string? accountId { get; set; }
+        public string? buildingId { get; set; }
         public string? roomNumber { get; set; }
     }
 }
