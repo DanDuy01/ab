0bb2865a86c321039d7e667ffc2a53e2e16e5070
haidang <machaidangms@gmail.com>
2024-03-11 13:25:46 +0700

    small change elevator conflict

--- ABMS_backend/Services/ElevatorService.cs
+++ ABMS_backend/Services/ElevatorService.cs
@@ -38,18 +38,7 @@ namespace ABMS_backend.Services
             }
             try
             {
-                bool hasConflict = _abmsContext.Elevators.Any(elevator =>
-    elevator.Status == 3 &&
-    (dto.start_time < elevator.EndTime && dto.end_time > elevator.StartTime));
-
-                if (hasConflict)
-                {
-                    return new ResponseData<string>
-                    {
-                        StatusCode = HttpStatusCode.Conflict,
-                        ErrMsg = "Time slot conflict"
-                    };
-                }
+               
                 Elevator elevator = new Elevator();
                 elevator.Id = Guid.NewGuid().ToString();
                 elevator.RoomId = dto.room_id;
@@ -91,6 +80,7 @@ namespace ABMS_backend.Services
             }
             try
             {
+              
                 Elevator elevator = _abmsContext.Elevators.Find(id);
                 if (elevator == null)
                 {
@@ -124,6 +114,19 @@ namespace ABMS_backend.Services
             if (elevator == null)
             {
                 throw new CustomException(ErrorApp.OBJECT_NOT_FOUND);
+            }
+            bool hasConflict = _abmsContext.Elevators.Any(otherElevator =>
+         otherElevator.Id != id &&
+         otherElevator.Status == 3 &&
+         (elevator.StartTime < otherElevator.EndTime && elevator.EndTime > otherElevator.StartTime));
+
+            if (hasConflict)
+            {
+                return new ResponseData<string>
+                {
+                    StatusCode = HttpStatusCode.Conflict,
+                    ErrMsg = "Time slot conflict"
+                };
             }
             elevator.Status = status;
             string getUser = Token.GetUserFromToken(_httpContextAccessor.HttpContext.Request.Headers["Authorization"]);
