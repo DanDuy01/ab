02974a9fcdb3339f0ed902492bd163057cad9217
haidang <machaidangms@gmail.com>
2024-03-13 12:30:41 +0700

    small change visitor get dto

--- ABMS_backend/DTO/VisitorForSearchDTO.cs
+++ ABMS_backend/DTO/VisitorForSearchDTO.cs
@@ -2,11 +2,11 @@
 {
     public class VisitorForSearchDTO
     {
-        public string roomId { get; set; }
-        public string fullName { get; set; }
-        public DateTime time { get; set; }
-        public string phoneNumber { get; set; }
-        public int status { get; set; }
+        public string? roomId { get; set; }
+        public string? fullName { get; set; }
+        public DateTime? time { get; set; }
+        public string? phoneNumber { get; set; }
+        public int? status { get; set; }
 
 
     }
