4b67fb45f3b5f357e5422b6ef474b5aace36a3e1
DanDuy <dcduy01@gmail.com>
2024-03-17 21:35:21 +0700

    feature: add ServiceType Management

---
+++ ABMS_backend/DTO/ServiceTypeDTO/ServiceTypeSearch.cs
@@ -0,0 +1,9 @@
+﻿namespace ABMS_backend.DTO.ServiceTypeDTO
+{
+    public class ServiceTypeSearch
+    {
+        public string? buildingId { get; set; }
+        public string? name { get; set; }
+        public int? status { get; set; }
+    }
+}
