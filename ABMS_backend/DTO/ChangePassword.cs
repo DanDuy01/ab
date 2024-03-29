59570429f5a6608e9bac8c6a3c393a1a38222c61
DanDuy <dcduy01@gmail.com>
2024-03-30 03:18:22 +0700

    add new function Changepassword

---
+++ ABMS_backend/DTO/ChangePassword.cs
@@ -0,0 +1,27 @@
+﻿namespace ABMS_backend.DTO
+{
+    public class ChangePassword
+    {
+        public string old_password { get; set; }
+        public string new_password { get; set; }
+        public string confirm_password { get; set; }
+
+        public string Validate()
+        {
+            if (string.IsNullOrEmpty(old_password))
+            {
+                return "Old password is required!";
+            }
+            else if (string.IsNullOrEmpty(new_password))
+            {
+                return "New password is required!";
+            }
+            else if (new_password != confirm_password)
+            {
+                return "Confirm password must be same the new password!";
+            }
+
+            return null;
+        }
+    }
+}
